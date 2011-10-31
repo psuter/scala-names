0904ad8293f94d3075c2c592e25602872157dd1d
ericZbinden <eric.zbinden@epfl.ch>
2011-10-31 10:34:45 +0100

    self recursion feature corrected for overloaded method names.

--- scala-names/src/main/scala/ch/epfl/lara/scalanames/features/ContainsSelfRecursion.scala
+++ scala-names/src/main/scala/ch/epfl/lara/scalanames/features/ContainsSelfRecursion.scala
@@ -6,13 +6,23 @@ trait ContainsSelfRecursion extends MethodFeature {
 
   val name = "Method contains a self-recursion."
   
-  def mkRecuTraverser(methodName: String) = new Traverser {
+  def mkRecuTraverser(methodDef: MethodDef) = new Traverser {
 	var foundRecu : Boolean = false
 
 	override def traverse(tree : Tree) : Unit = {
 	  if(!foundRecu) {
       	tree match {
-      	    case Apply(Select(This(tn),name),args) if(methodName==name.toString())=> foundRecu = true
+      	    case Apply(Select(This(tn),name),args) if(methodDef.name==name.toString())=> {
+      	     
+      	      val params = methodDef.d.symbol.paramss
+      	      if(params.isEmpty){
+      	        if(args.isEmpty) foundRecu = true
+      	        else super.traverse(tree)
+      	      } else {
+      	        if(verifyArgs(args,params.head)) foundRecu= true
+      	        else super.traverse(tree)
+      	      }
+      	    }
       	    case _ => super.traverse(tree)
       	}
       }
@@ -20,9 +30,16 @@ trait ContainsSelfRecursion extends MethodFeature {
   }
 
   def appliesTo(methodDef : MethodDef) : Boolean = {
-    val recursifTraverser = mkRecuTraverser(methodDef.name)
+    val recursifTraverser = mkRecuTraverser(methodDef)
     recursifTraverser.traverse(methodDef.d.rhs)
     recursifTraverser.foundRecu
+  }
+  
+  private def verifyArgs(args: List[Tree],params: List[Symbol]): Boolean = args match {
+    case x :: Nil if(!params.isEmpty)=> args.head.tpe.resultType =:= params.head.tpe
+    case x :: xs if(!params.isEmpty)=> args.head.tpe.resultType =:= params.head.tpe && verifyArgs(args.tail,params.tail)
+    case Nil => params.isEmpty //no param = true, else wrong function
+    case _ => false
   }
   
   
