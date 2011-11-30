0e1cadc8243aa3eae64689099b8720ca5df448ea
ericZbinden <eric.zbinden@epfl.ch>
2011-11-30 15:44:48 +0100

    special characters are now consider as blank

--- scala-names/src/main/scala/ch/epfl/lara/scalanames/features/IsCamelPhrase.scala
+++ scala-names/src/main/scala/ch/epfl/lara/scalanames/features/IsCamelPhrase.scala
@@ -17,16 +17,18 @@ trait IsCamelPhrase extends MethodFeature {
   //Split camel phrase
   //return empty list if contains other things than letter or digit
   def splitWord(name: String, current: String, res: List[String]): List[String] = {
-    if (name.isEmpty) res:::List(current)
+    
+    def cur = if(current=="")res else res:::List(current)
+    
+    if (name.isEmpty) cur
     else {
       val head = name.head
-      if (head.isLetter){
-        if(head.isUpperCase) splitWord(name.tail,head.toString,if(current=="")res else res:::List(current))
-        else splitWord(name.tail,current+head,res)  
-      } else if (head.isDigit){
-        splitWord(name.tail,head.toString(),res:::List(current))
-      }
-      else List()
+      if (head.isLetter){ 												//a letter
+        if(head.isUpperCase) splitWord(name.tail,head.toString,cur) 	//uppercase letter
+        else splitWord(name.tail,current+head,res)  			    	//lowercase letter
+      } else if (head.isDigit) splitWord(name.tail,head.toString,cur)	//a digit      
+      else splitWord(name.tail,"",cur)									//a special character
+      
     }
   }
 
