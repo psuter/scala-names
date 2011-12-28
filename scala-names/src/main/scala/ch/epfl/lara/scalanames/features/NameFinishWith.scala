0ec252bcb6b6631e474a314c7be31a59618245bd
ericZbinden <eric.zbinden@epfl.ch>
2011-12-28 12:44:51 +0100

    added NameFinishWith RightAssociative now extend NameFinishWith

---
+++ scala-names/src/main/scala/ch/epfl/lara/scalanames/features/NameFinishWith.scala
@@ -0,0 +1,18 @@
+package ch.epfl.lara.scalanames.features
+
+trait NameFinishWith extends MethodFeature {
+  import component._
+  import component.global._
+  
+  val pattern : String
+  
+  val name = "This method finish with "+pattern
+    
+  def appliesTo(methodDef: MethodDef): Boolean = pattern.length match {
+    case 0 => false
+    case 1 => methodDef.name.last == pattern.head
+    case 2 if(methodDef.name.length > 1) => methodDef.name.takeRight(2) == pattern
+    case x if(methodDef.name.length > (x-1)) => methodDef.name.takeRight(x) == pattern
+    case _ => false
+  }
+}
--- scala-names/src/main/scala/ch/epfl/lara/scalanames/features/RightAssociative.scala
+++ scala-names/src/main/scala/ch/epfl/lara/scalanames/features/RightAssociative.scala
@@ -1,11 +1,12 @@
 package ch.epfl.lara.scalanames.features
 
-trait RightAssociative extends MethodFeature {
+trait RightAssociative extends NameFinishWith {
   import component._
   import component.global._
 
-  val name = "This method is right associative"
+  override val name = "This method is right associative"
     
-  def appliesTo(methodDef: MethodDef): Boolean = methodDef.name.last==':'
+  override val pattern = ":"
+    
 
 }
