e38bae4f3ec8553d26195072a4379285c14beae4
ericZbinden <eric.zbinden@epfl.ch>
2012-01-01 16:16:21 +0100

    top level method are no more considered as inner method

--- scala-names/src/main/scala/ch/epfl/lara/scalanames/features/ContainsInnerMethod.scala
+++ scala-names/src/main/scala/ch/epfl/lara/scalanames/features/ContainsInnerMethod.scala
@@ -1,14 +1,30 @@
 package ch.epfl.lara.scalanames.features
 
-trait ContainsInnerMethod extends ContainsTraversalFeature {
+trait ContainsInnerMethod extends MethodFeature {
   import component._
   import component.global._
   
   val name = "Method body contains method definition"
   
-  def mkPattern(tree: Tree): Boolean = tree match {
-    case DefDef(mods, name, tparams, vparamss, tpt, rhs) => true
-    case _ => false
+   def mkDefTraverser = new Traverser {
+	var foundDef : Boolean = false
+
+	override def traverse(tree : Tree) : Unit = {
+	  if(!foundDef) {
+      	tree match {
+      	  case cd : ClassDef => 
+      	  case dd : DefDef => foundDef = true
+      	  //traverse deeper
+      	  case _ => super.traverse(tree)
+      	}
+      }
+    }
+  }
+
+  def appliesTo(methodDef : MethodDef) : Boolean = {
+    val defTraverser = mkDefTraverser
+    defTraverser.traverse(methodDef.d.rhs)
+    defTraverser.foundDef
   }
 
 }
